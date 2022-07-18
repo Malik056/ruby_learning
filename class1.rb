9c27473b60b6aa5aacfab08dfe2fefbfa78dd639
Muhammad Taha <tahamahmoodmalik@gmail.com>
2022-07-18 10:18:46 +0500

    modules practice

--- class1.rb
+++ class1.rb
@@ -4,3 +4,44 @@ end
 
 puts sum(3,4)
 puts sum("cat","dog")
+
+
+a = [2,3,4,5,6,7]
+b = a.map do
+    |element| 
+    element.class
+end
+
+puts b
+puts "PUTTING C"
+puts "PUTTING C"
+puts "PUTTING C"
+c = a.map &:to_s
+puts c
+
+
+
+
+module ModA
+    def log
+        puts "Logging in methods"
+    end
+end
+
+module ModB
+    def log
+        puts "Logging in methods ModB"
+    end
+end
+
+class A
+    extend ModA
+    include ModA
+    include ModB
+end
+
+puts A.ancestors
+
+variable = A.new
+A.log
+variable.log
